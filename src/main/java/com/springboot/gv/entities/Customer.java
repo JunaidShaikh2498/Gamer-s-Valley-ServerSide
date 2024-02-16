0e423fc8671a4e96ce599ee2d98ed7899657ddd2
hriishiii <hrishikeshbhogade42@gmail.com>
2024-02-16 09:28:40 +0530

    changes done

--- src/main/java/com/springboot/gv/entities/Customer.java
+++ src/main/java/com/springboot/gv/entities/Customer.java
@@ -13,10 +13,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+
 @Entity
 @Table(name="Customers")
 public class Customer {
